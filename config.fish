6e8efc00832966a058025a5cb8841d99befaec93
anousone <anousone.litd@gmail.com>
2022-10-30 11:20:09 +0700

    add fish config

---
+++ config.fish
@@ -0,0 +1,26 @@
+alias ls 'exa -l --group-directories-first --git --no-user --no-permissions --icons -s=extension'
+alias ll 'exa -l --group-directories-first -h --git --icons -s=extension -a'
+alias svim 'vim -u ~/.SpaceVim/vimrc'
+alias p 'prevd'
+alias n 'nextd'
+alias gst 'git status'
+alias gg 'lazygit'
+alias n 'nvim'
+
+bind \cf 'forward-char'
+# bind \ct fzf_file
+# bind \cr fzf_command_history
+set -U FZF_CTRL_R_OPTS "--reverse"
+set -U FZF_TMUX_OPTS "-p"
+
+# git log --graph --date-order --date=short --pretty=format:'%C(cyan)%h %C(blue)%ar%C(auto)%d %C(yellow)%s%+b %C(black)%ae'
+
+starship init fish | source
+fish_add_path $HOME/.config/bin
+
+# git
+
+if status is-interactive && test -f ~/.config/fish/custom/git_fzf.fish
+	source ~/.config/fish/custom/git_fzf.fish
+	git_fzf_key_bindings
+end
